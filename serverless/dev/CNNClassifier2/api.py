03bafb8d213b8505ca651003d3ba89162687f9fb
Chen Li <yichen.li0830@gmail.com>
2017-10-16 14:39:58 -0700

    Create learning curve

--- serverless/dev/CNNClassifier2/api.py
+++ serverless/dev/CNNClassifier2/api.py
@@ -5,9 +5,11 @@ import numpy as np
 import python_speech_features
 import scipy.io.wavfile as wav
 from keras import initializers, backend as K
-from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Flatten, Activation
 from keras.layers import Conv2D, MaxPooling2D, Input, AveragePooling2D
+from keras import optimizers
+from keras.models import Sequential, Model
+from keras.layers import Dense, Dropout, Flatten, Activation, Conv2D, MaxPooling2D
+from keras.callbacks import EarlyStopping, ModelCheckpoint
 import datetime
 from pyAudioAnalysis import audioFeatureExtraction
 import librosa
@@ -16,7 +18,157 @@ import scipy
 import config
 from keras.preprocessing.image import ImageDataGenerator
 
+def trainNetwork(X_train, Y_train_cat, X_valid, Y_valid_cat, X_test, Y_test_cat, architecture, num_classes, kernalSize,
+                 batch_size, epochs, class_weight_dict):
 
+    # Build modal
+    if architecture == 0:
+        lastModel = Sequential()
+        lastModel.add(Dense(units=num_classes, input_dim=X_train.shape[1], activation='softmax'))
+        lastModel.compile(loss='categorical_crossentropy',
+                          # optimizer=adam,
+                          # optimizer='rmsprop',
+                          optimizer='adam',
+                          metrics=['accuracy'])
+
+    elif architecture == 1:
+        lastModel = Sequential()
+        print("x_all.shape[1]", X_train.shape[1])
+        lastModel.add(Dense(units=8, input_dim=X_train.shape[1], activation='relu'))
+        lastModel.add(Dense(units=4, activation='relu'))
+        # lastModel.add(Dense(units=4, input_dim=4))
+        # lastModel.add(Dense(units=8, input_dim=8))
+        lastModel.add(Dense(units=num_classes, activation='softmax'))
+        # lastModel.add(Activation('softmax'))
+        # print()
+        lastModel.compile(loss='categorical_crossentropy',
+                          # optimizer=adam,
+                          # optimizer='rmsprop',
+                          optimizer='adam',
+                          metrics=['accuracy'])
+
+    elif architecture == 2:
+        lastModel = Sequential()
+        lastModel.add(Dense(units=num_classes, input_dim=kernalSize))
+        # lastModel.add(Dense(units=512, input_dim=kernalSize))
+        # lastModel.add(Dense(units=num_classes, input_dim=512))
+        lastModel.add(Activation('softmax'))
+        # lastModel.add(Dense(10, activation='softmax'))
+
+        # sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+        # adam = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+
+        lastModel.compile(loss='categorical_crossentropy',
+                          # optimizer=adam,
+                          optimizer='rmsprop',
+                          # optimizer='adam',
+                          metrics=['accuracy'])
+
+    elif architecture == 3:
+        lastModel = Sequential()
+        print("x_all.shape[1]", X_train.shape[1])
+        lastModel.add(Dense(units=32, input_dim=X_train.shape[1], activation='relu'))
+        lastModel.add(Dense(units=16, activation='relu'))
+        lastModel.add(Dense(units=8, activation='relu'))
+        # lastModel.add(Dense(units=4, input_dim=4))
+        # lastModel.add(Dense(units=8, input_dim=8))
+        lastModel.add(Dense(units=num_classes, activation='softmax'))
+        # lastModel.add(Activation('softmax'))
+        # print()
+        lastModel.compile(loss='categorical_crossentropy',
+                          # optimizer=adam,
+                          # optimizer='rmsprop',
+                          optimizer='adam',
+                          metrics=['accuracy'])
+
+    elif architecture == 4:
+        lastModel = Sequential()
+        # print("x_all.shape[1]", X_train.shape[1])
+        # x_all.reshape(-1, 128, 1500, 1)
+
+        # lastModel.add(Conv2D(64, (3, 3), padding="same"))
+        # lastModel.add(Activation('relu'))
+        # lastModel.add(MaxPooling2D(pool_size=(2, 2), padding="same"))
+
+        lastModel.add(Conv2D(32, (3, 3), input_shape=(X_train[0].shape[0], X_train[0].shape[1], 1), padding="same"))
+        lastModel.add(MaxPooling2D(pool_size=(2, 2), padding="same"))
+        lastModel.add(Activation('relu'))
+
+        lastModel.add(Conv2D(32, (3, 3), padding="same"))
+        lastModel.add(MaxPooling2D(pool_size=(2, 2), padding="same"))
+        lastModel.add(Activation('relu'))
+
+        lastModel.add(Conv2D(64, (3, 3), padding="same"))
+        lastModel.add(MaxPooling2D(pool_size=(2, 2), padding="same"))
+        lastModel.add(Activation('relu'))
+
+        lastModel.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+        lastModel.add(Dense(128))
+        lastModel.add(Activation('relu'))
+        lastModel.add(Dropout(0.5))
+        lastModel.add(Dense(units=num_classes, activation='softmax'))
+
+        lastModel.compile(loss='categorical_crossentropy',
+                          # optimizer=adam,
+                          # optimizer='rmsprop',
+                          optimizer='adam',
+                          metrics=['accuracy'])
+
+
+    # callbacks
+    esCallback = EarlyStopping(monitor='val_loss',
+                               min_delta=0,
+                               patience=10,
+                               verbose=1, mode='auto')
+
+    saveModelFilePath = "savedModel"
+    if os.path.exists(saveModelFilePath) != True:
+        print("Creating dir: " + saveModelFilePath)
+        os.makedirs(saveModelFilePath)
+
+    savedModelPath = saveModelFilePath + "/weights.best.hdf5"
+
+    checkpoint = ModelCheckpoint(savedModelPath, monitor='val_loss', verbose=1, save_best_only=True,
+                                 save_weights_only=False,
+                                 mode='auto', period=1)
+
+    # fit model
+    lastModel.fit(X_train, Y_train_cat, batch_size=batch_size, epochs=epochs, verbose=1,
+                  validation_data=(X_valid, Y_valid_cat), class_weight=class_weight_dict,
+                  callbacks=[checkpoint, esCallback])
+
+    print("Loading best model weight...")
+    lastModel.load_weights(savedModelPath)
+    # Test modal
+    score = lastModel.evaluate(X_test, Y_test_cat, verbose=0)
+    unweightedLoss = score[0]
+    unweightedAcc = score[1]
+    print('Test loss:', unweightedLoss)
+    print('Test Unweighted accuracy:', unweightedAcc)
+
+    # y_pred = lastModel.predict(X_test)
+    # acc = sum([np.argmax(Y_test_cat[i])==np.argmax(y_pred[i]) for i in range(len(X_test))])/len(X_test)
+    # print('Real test accuracy:', acc)
+    y_pred = lastModel.predict(X_test)
+    weightedAccuracyArr = np.zeros(num_classes)
+    count = np.zeros(num_classes)
+    for i in range(len(X_test)):
+        categoryIdx = np.argmax(Y_test_cat[i])
+        weightedAccuracyArr[categoryIdx] += np.argmax(Y_test_cat[i]) == np.argmax(y_pred[i])
+        count[categoryIdx] += 1
+
+    weightedAccuracyArr = weightedAccuracyArr / count
+    weightedAcc = sum(weightedAccuracyArr) / num_classes
+
+    print("Weighted category counts", count)
+    print("WeightedAccuracyArr", weightedAccuracyArr)
+    print("Weighted Accuracy: ", weightedAcc)
+
+    # Save modal
+    lastModel.save('emotion_model.h5')  # creates a HDF5 file 'my_model.h5'
+    lastModel.save_weights('emotion_model_weights.h5')
+
+    return unweightedAcc, weightedAcc
 
 def placeUtteranceToFolder(wavPath, category, savePath):
     catePath = savePath + "/" + category
@@ -124,7 +276,6 @@ def generateData(x, y, batch_size=4, width_shift_range=0.2, zoom_range=0.05):
     return x_generated, y_generated
     # x_all = x_generated
     # y_all = y_generated
-
 
 
 def getMelspectrogram(wavPath):
@@ -285,7 +436,7 @@ def calculate_XY(wavDirBase, categories, kernalSize, numOfWavsForEachCategory=-1
                 result = getMelspectrogram(wavPath)
                 # print("result", result)
 
-            if len(result)==0:
+            if len(result) == 0:
                 continue
 
             # print("result.shape", result.shape)
