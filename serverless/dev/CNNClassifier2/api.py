8a6c735ef91efd450d56875fff35c9dc2b2f0481
Chen Li <yichen.li0830@gmail.com>
2017-10-15 22:27:11 -0700

    tuning model 2

--- serverless/dev/CNNClassifier2/api.py
+++ serverless/dev/CNNClassifier2/api.py
@@ -135,7 +135,7 @@ def getMelspectrogram(wavPath):
     # # Features: mel-spectrogram
     # features = librosa.feature.melspectrogram(y=sig, sr=rate, fmin=50, fmax=3000)
 
-    if (sig.shape[0] / rate > 5) or (sig.shape[0] / rate < 3):
+    if (sig.shape[0] / rate > 7) or (sig.shape[0] / rate < 3):
         return []
 
     features = librosa.feature.melspectrogram(y=sig, sr=rate, fmin=50, fmax=3000)
