6b5ad80193305a203f3ce9d78a53274efdca1fa8
Chen Li <yichen.li0830@gmail.com>
2017-11-04 12:04:49 -0700

    Remove unused models and classifiers

--- serverless/dev/autoClassifier/init.sh
+++
@@ -1,2 +0,0 @@
-pip install librosa
-wget https://s3-us-west-2.amazonaws.com/clevo.data/audio.zip
--- serverless/dev/test.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-let file = fs.readFileSync('/Users/Chen/百度云同步盘/Startup/Clevo/联动数据/录音wav/20170630103142_966_13841314049_601.wav');
-
-console.log("file", file);
